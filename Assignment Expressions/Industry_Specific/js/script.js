e5ff197491a15af728eb48343b110de49968007f
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-03 17:21:57 -0500

    beginning the project

--- Assignment Expressions/Industry_Specific/js/script.js
+++ Assignment Expressions/Industry_Specific/js/script.js
@@ -1,4 +1,13 @@
 // EXPRESSIONS ASSIGNMENT INDUSTRY-SPECIFIC
+// Harmony Cornett November 3, 2013
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "white";
+
+//This will demonstrate how to calculate the price of a website with the number of hours spent with the web designer.
+
+var pricePerHour = 50.00;
+var hoursSpent = 15;
+var totalCostOfSite = pricePerHour * totalCostOfSite;
+
+console.log("We spent " + hoursSpent + " hours on your website/. Your total cost will be " + totalCostOfSite.);
