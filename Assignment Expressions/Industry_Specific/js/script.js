8138be8d3e80ff0cfd5ad204e604b8fa383c6804
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-03 17:33:06 -0500

    labeling all of the syntax

--- Assignment Expressions/Industry_Specific/js/script.js
+++ Assignment Expressions/Industry_Specific/js/script.js
@@ -6,8 +6,12 @@ document.querySelector("body").style.backgroundColor= "white";
 
 //This will demonstrate how to calculate the price of a website with the number of hours spent with the web designer.
 
+//This is the price I charge per hour of work on her website, it is 50.00 per hour.
 var pricePerHour = 50;
+//this is my variable to say how many hours we spent on the website design.
 var hoursSpent = 15;
+//This is the calculation of 50.00 per hour multiplied by 15 hours spent, to give the total cost of the website that I developed for the client.
 var totalCostOfSite = (pricePerHour * hoursSpent);
 
+//This writes to the console "We spent 15 hours on your website.  Your total cost for the website is $750." It calculates the total cost of the website with the variables above.
 console.log("We spent " + hoursSpent + " hours on your website." + " Your total cost for the website is $" + totalCostOfSite + ".");
