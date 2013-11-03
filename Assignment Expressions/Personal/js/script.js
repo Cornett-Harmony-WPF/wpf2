3e47f53ac56921b1d160ea339b19a58362271cb1
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-03 17:58:21 -0500

    birthday issues

--- Assignment Expressions/Personal/js/script.js
+++
@@ -1,4 +0,0 @@
-// Expressions Assignment PERSONAL
-
-//this changes the css of your body tag to be green"white";
-
---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,14 @@
+// Expressions Assignment PERSONAL
+//Harmony Cornett November 3, 2103
+
+//This will calculate my schedule of birthdays on my personal calendar, so that I can keep track of birthdays and how many days apart they are.
+
+var today, month, txt;
+today = new Date();
+month = ['Jan','Feb','Mar','Apr','May','Jun',
+  'Jul','Aug','Sep','Oct','Nov','Dec'];
+friendsBday = [01];
+momsBday = [04];
+issue = friendsBday - momsBday
+issue2 = friendsBday - today
+console.log("what is the birthday issue? "  + issue + "and" + issue2);
