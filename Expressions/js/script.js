5fb6d8f4a6ada990c0e6aecee7e2957b21f13a6d
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-02 23:05:12 -0400

    expressions screencast
    pemdas

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,6 +1,12 @@
 // Expressions
 
-var a = 2;//sets up our variable a and defines it with 2
-a = a + 3;//add 3 to a
-console.log(a+3);
+//var a = 2;//sets up our variable a and defines it with 2
+//a = a + 3;//add 3 to a
+//console.log(a+3);
+
+
+var yearBorn = 1982;
+var age = 2013 - yearBorn - 1;
+console.log(age);
+
 
