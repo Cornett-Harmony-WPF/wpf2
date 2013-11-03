ab2b70c5bdfa464a458abbb0d838e86c5b33ddb5
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-03 18:04:13 -0500

    starting over

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,11 +1,11 @@
 // Expressions Assignment PERSONAL
 //Harmony Cornett November 3, 2103
 
-//This will calculate my schedule of birthdays on my personal calendar, so that I can keep track of birthdays and how many days apart they are.
+//This will calculate my schedule of events on my personal calendar, so that I can keep track of birthdays and how many days apart they are.
 
 var today, month, txt;
-today = new Date();
-month = ['Jan','Feb','Mar','Apr','May','Jun',
+today = new array();
+['Jan','Feb','Mar','Apr','May','Jun',
   'Jul','Aug','Sep','Oct','Nov','Dec'];
 friendsBday = [01];
 momsBday = [04];
