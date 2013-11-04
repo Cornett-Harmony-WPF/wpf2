ab03901dc6abddd9f7d48e7a2359818b90296260
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 09:10:58 -0500

    dreaming up my issue

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,4 +4,8 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "blue";
 
-//This 
+//This will be a calculation of how many gallon buckets of paint I will need to paint a 4 wall, 16ft by 16ft room in my house.
+
+var gallonBucketsperWall;
+var totalGallonBuckets;
+var gallon
