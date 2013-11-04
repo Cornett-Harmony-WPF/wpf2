26e80837544a6d96dd0a8abdb474bf6fa49246dc
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 15:14:28 -0500

    following along with relational conditional screencast
    This was the original Conditionals Document.

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,13 +1,13 @@
-// Basic Conditional Statement
+// Basic Conditional Statement- Relational Expressions
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF11";
 
-var oldEnough = true;
-
+var kidHeight = 40;
+var minHeight = 48;
 //if the child is old enough, print to the console "You can ride!"
-if(oldEnough){
+//if the kid os over 48 inches in height
+if(kidHeight > 48){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
 }
-console.log("what comes after");
