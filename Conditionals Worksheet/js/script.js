bb0f546d8aeb65b2abe39ee3616f899935543702
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 16:27:32 -0500

    Conditionals Worksheet

---
+++ Conditionals Worksheet/js/script.js
@@ -0,0 +1,16 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF11";
+//This is the variable of the competitor's weight
+var weight = 265;
+//this is the if condition
+if(weight >= 250){
+//This is what is the result of the if statement combined with the variable.
+	console.log("The competitor qualifies for the heavyweight division.");
+}else{
+//If the first statement is not true it will register as false and this will occur.
+	console.log("The competitor needs to gain some weight.");
+}
+
+
