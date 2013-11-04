0224e7a716e693e93239764fad757348dd248402
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 09:03:46 -0500

    finishing my array

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,14 +1,39 @@
 // Expressions Assignment PERSONAL
 //Harmony Cornett November 3, 2103
 
-//This will calculate my schedule of events on my personal calendar, so that I can keep track of birthdays and how many days apart they are.
+//This will calculate how many miles I drive in a month.
+var monthName = new Array();
+monthName[0] = "none"
+monthName[1] = "January"
+monthName[2] = "Febuary"
+monthName[3] = "March"
+monthName[4] = "April"
+monthName[5] = "May"
+monthName[6] = "June"
+monthName[7] = "July"
+monthName[8] = "August"
+monthName[9] = "September"
+monthName[10] = "October"
+monthName[11] = "November"
+monthName[12] = "December"
 
-var today, month, txt;
-today = new array();
-['Jan','Feb','Mar','Apr','May','Jun',
-  'Jul','Aug','Sep','Oct','Nov','Dec'];
-friendsBday = [01];
-momsBday = [04];
-issue = friendsBday - momsBday
-issue2 = friendsBday - today
-console.log("what is the birthday issue? "  + issue + "and" + issue2);
+var month = new Array();
+month[0] = 0
+month[1] = 31
+month[2] = 31
+month[3] = 31
+month[4] = 30
+month[5] = 31
+month[6] = 30
+month[7] = 31
+month[8] = 31
+month[9] = 30
+month[10] = 31
+month[11] = 30
+month[12] = 31
+var milesPerDay = 75;
+var inputMonth = 4;
+var milesPerMonth= milesPerDay * month[inputMonth];
+
+
+console.log("I drive " + milesPerMonth + " miles in the month of " + monthName[inputMonth] + ".");
