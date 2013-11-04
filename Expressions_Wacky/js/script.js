807db9cbf849698f554a7580313c8ebcc4983955
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 09:22:29 -0500

    figuring out the math portion of this JavaScript

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,7 +5,10 @@
 document.querySelector("body").style.backgroundColor= "blue";
 
 //This will be a calculation of how many gallon buckets of paint I will need to paint a 4 wall, 16ft by 16ft room in my house.
+var wallSize = 20;
+var walls = 4;
+var wallAreas = walls * wallSize * gallonCovers;
+var gallonCovers = 20;
+var totalGallonBucketsNeeded = wallAreas * gallonCovers;
 
-var gallonBucketsperWall;
-var totalGallonBuckets;
-var gallon
+console.log("Each gallon bucket of paint covers " + gallonCovers + " feet of wall space." + " So, you should need " + totalGallonBucketsNeeded + " gallon buckets of paint for your " + wallAreas + " feet of wall space.");
