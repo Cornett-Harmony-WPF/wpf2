ba90bd8f3890c9213386b9b574ce80c084267c99
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 15:47:33 -0500

    following along with screencast if and else statements

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,11 +3,15 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF11";
 
-var kidHeight = 40;
-var minHeight = 48;
+var kidHeight = 48;
+var minHeight = 52;
+
 //if the child is old enough, print to the console "You can ride!"
 //if the kid os over 48 inches in height
-if(kidHeight > 48){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+}
+if(kidHeight <= minHeight){
+		console.log("Sorry kid you've got some growing to do first");
 }
