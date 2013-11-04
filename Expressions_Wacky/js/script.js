527446cb5143b6db882386176c6ebdb8de28afd9
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 09:08:48 -0500

    creating my wacky expressions page

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,7 @@
 // ASSIGNMENT EXPRESSIONS WACKY
+//Harmony Cornett November 4, 2013 
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "blue";
+
+//This 
