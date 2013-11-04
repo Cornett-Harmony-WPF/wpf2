97a0dcbc4b0cc0a1abb4923772beaf9038dda509
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 15:10:14 -0500

    following along witht the screencast for CONDITITIONALS

---
+++ Conditionals/js/script.js
@@ -0,0 +1,13 @@
+// Basic Conditional Statement
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF11";
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "You can ride!"
+if(oldEnough){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+console.log("what comes after");
