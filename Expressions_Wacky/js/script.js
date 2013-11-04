56a5c973cc4e5a7fd290ce744c0ffda1cc61ac07
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 09:24:04 -0500

    chaning the order of the variables

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,8 +5,8 @@
 document.querySelector("body").style.backgroundColor= "blue";
 
 //This will be a calculation of how many gallon buckets of paint I will need to paint a 4 wall, 16ft by 16ft room in my house.
-var wallSize = 20;
 var walls = 4;
+var wallSize = 20;
 var gallonCovers = 20;
 var wallAreas = walls * wallSize * gallonCovers;
 var totalGallonBucketsNeeded = wallAreas * gallonCovers;
