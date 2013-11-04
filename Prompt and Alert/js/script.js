19ae5f50dac7f461e23597708caf28ad5ca517be
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 10:21:23 -0500

    screencast following along

--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -3,5 +3,14 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "black";
 
+var width = prompt("We are calculating the area of a rectangle.  \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area = width * height; //calculates with that information.
+console.log(area); //Prints it out to the console.
 
+
+alert("The area of your rectangle is " + area + " sq feet");
+
+or var result = '''''''''''''
+alert(result);
 
