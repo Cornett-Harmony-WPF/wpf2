80bfb3998bfa44d5b70e089469369631dd0d64ad
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 20:42:00 -0500

    finished with the assignment

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -44,3 +44,22 @@ else if(username === "jj"){
 }else{
 	console.log("Password does not match our records");
 }
+
+
+//Tire Pressure I
+
+var tirePressure = new Array ();
+tirePressure[0] = 32;
+tirePressure[1] = 56;
+tirePressure[2] = 28;
+tirePressure[3] = 17;
+var avgTirePressure = 32
+
+if(tirePressure >= 32){
+//This is what is the result of the if statement combined with the variable.
+	console.log("The tires pass spec!");
+}else{
+//If the first statement is not true it will register as false and this will occur.
+	console.log("Get your tires checked out!");
+}
+
