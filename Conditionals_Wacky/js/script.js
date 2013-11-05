f660cc48292a17338f2454f195198b4efa06931d
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 21:48:10 -0500

    beginning assignment

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,9 @@
+// Conditionals Wacky
+// Harmony Cornett November 3, 2013
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF11";
+
+//This will calculate whether or not I have enough in my budget, after bills, to buy a flat-screen TV.
+
+var
