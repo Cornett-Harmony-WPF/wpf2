defe5e7320477480a94fdc05205ff33fa35bee4a
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 22:51:23 -0500

    starting over

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,14 +3,11 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF11";
 
-//Tire Pressure I
+//How many boxes of girl scout cookies do we need to sell to beat our neighbors?
 
-var tirePressure = new Array ();
-tirePressure[0] = 32;
-tirePressure[1] = 56;
-tirePressure[2] = 28;
-tirePressure[3] = 17;
-var avgTirePressure = 32
+var totalBoxes = 25;
+var neighborsSold = 16;
+var theDifference = totalBoxes - neighborsSold;
 
 if(tirePressure >= 32){
 //This is what is the result of the if statement combined with the variable.
