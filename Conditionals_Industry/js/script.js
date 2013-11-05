f1c974119e8b95bef1c4a14f07e5adf5a22a5e72
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 22:48:18 -0500

    beginning with old example notes from a screencast

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,3 +2,20 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF11";
+
+//Tire Pressure I
+
+var tirePressure = new Array ();
+tirePressure[0] = 32;
+tirePressure[1] = 56;
+tirePressure[2] = 28;
+tirePressure[3] = 17;
+var avgTirePressure = 32
+
+if(tirePressure >= 32){
+//This is what is the result of the if statement combined with the variable.
+	console.log("The tires pass spec!");
+}else{
+//If the first statement is not true it will register as false and this will occur.
+	console.log("Get your tires checked out!");
+}
