000e67a7926fb2df261bd88995cc2a117724b232
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 22:21:55 -0500

    starting over again!

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,3 +6,7 @@ document.querySelector("body").style.backgroundColor= "#00FF11";
 
 //This will calculate whether or not I have enough in my budget, after bills, to buy a flat-screen TV.
 
+var totalbills = 1360.00;
+var totalIncome = 1500.00;
+var costOfTv = 500.00;
+
