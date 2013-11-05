57d3175e52d159ee81b46f4072597d703db94876
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-04 22:44:50 -0500

    finally getting this finished

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,8 +5,19 @@
 document.querySelector("body").style.backgroundColor= "#00FF11";
 
 //This will calculate whether or not I have enough in my budget, after bills, to buy a flat-screen TV.
-
-var totalbills = 1360.00;
+//this is my totalBills variable, it shows the money going out.
+var totalBills = 1400;
+//this is my totalIncome variable, it shows my money coming in.
 var totalIncome = 1500.00;
+//this is the actual cost of the tv variable.  It is 500 dollars.
 var costOfTv = 500.00;
+//this is the 
+var leftOver = totalIncome - totalBills;
 
+if(leftOver >= 500){
+//This is what is the result of the if statement combined with the variable.
+	console.log("You have " + totalBills + " going out, you have " + totalIncome + " coming in, so you can buy the TV!");
+}else{
+//If the first statement is not true it will register as false and this will occur.
+	console.log("You have " + totalBills + " going out, you have " + totalIncome + " coming in, so keep Saving!");
+}
