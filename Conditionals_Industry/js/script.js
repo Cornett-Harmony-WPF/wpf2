a3236f083c6ec7545f477eb75eed6badea07e299
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-14 21:34:43 -0500

    still testing

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,22 +6,22 @@ document.querySelector("body").style.backgroundColor= "blue";
 //Who has RSVP'd and how many still need to, to attend the business office party?
 
 var totalInvites = new Array();
-totalInvites[0] = "Viola";
-totalInvites[1] = "Symphony";
-totalInvites[2] = "Sonata";
-totalInvites[3] = "Melody";
-totalInvites[4] = "Aria";
-totalInvites[5] = "Seattle";
-totalInvites[6] = "Sophia";
-totalInvites[7] = "Daisy";
-totalInvites[8] = "Erica";
-totalInvites[9] = "Rhapsody";
+totalInvites[0] = "Viola ";
+totalInvites[1] = "Symphony ";
+totalInvites[2] = "Sonata ";
+totalInvites[3] = "Melody ";
+totalInvites[4] = "Aria ";
+totalInvites[5] = "Seattle ";
+totalInvites[6] = "Sophia ";
+totalInvites[7] = "Daisy ";
+totalInvites[8] = "Erica ";
+totalInvites[9] = "Rhapsody ";
 
 var totalRsvp = new Array();
-totalRsvp[0] = "Rhapsody"
-totalRsvp[1] = "Melody"
-totalRsvp[2] = "Symphony"
-totalRsvp[3] = "Sonata"
+totalRsvp[0] = "Rhapsody "
+totalRsvp[1] = "Melody "
+totalRsvp[2] = "Symphony "
+totalRsvp[3] = "Sonata "
 
 var rsvpdAlready = 4;
 var stillNeedToRsvp = totalRsvp - totalInvites;
