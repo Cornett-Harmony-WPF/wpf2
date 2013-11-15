39e0de4c59614bf4ad2661c8b17dc55e2356031a
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-14 20:43:27 -0500

    creating and editing

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,4 +1,12 @@
 // JavaScript
 
 //this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF11";
+document.querySelector("body").style.backgroundColor= "red";
+
+//This will be about how many people to invite to a party vs. how many have already been invited.
+
+var invited = 80
+var maxAllowed = 200
+var stillNeedToInviet = maxAllowed - invited
+
+concole.log("I want to invite
