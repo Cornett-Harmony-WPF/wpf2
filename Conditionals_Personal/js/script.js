a64c0b8f242e1e440a9e5537a6703c1d6272e113
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-14 20:46:00 -0500

    explaining the problem in detail

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,7 +3,7 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "red";
 
-//This will be about how many people to invite to a party vs. how many have already been invited.
+//This will be about how many people to invite to a party vs. how many have already been invited.  Max occupancy is 200 people.  I have only x friends.  How many people do I have left to invite.  Based on how many friends you have will be the number of people you will need to invite, all the while making sure you don't go over the max amount of people for the club.  Whew!  Hope I explained that well.
 
 var invited = 80
 var maxAllowed = 200
