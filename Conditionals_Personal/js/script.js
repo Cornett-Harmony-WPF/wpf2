faf6da52d368f0d046f56c2826b4b81ddf0fe8dd
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-14 20:47:01 -0500

    adding appropriate semi-colans

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,8 +5,9 @@ document.querySelector("body").style.backgroundColor= "red";
 
 //This will be about how many people to invite to a party vs. how many have already been invited.  Max occupancy is 200 people.  I have only x friends.  How many people do I have left to invite.  Based on how many friends you have will be the number of people you will need to invite, all the while making sure you don't go over the max amount of people for the club.  Whew!  Hope I explained that well.
 
-var invited = 80
-var maxAllowed = 200
-var stillNeedToInviet = maxAllowed - invited
+var howManyFriendsYouHave;
+var invited = 80;
+var maxAllowed = 200;
+var stillNeedToInviet = maxAllowed - invited;
 
 concole.log("I want to invite
