c6e70eb49f747fa00a58a7cfc6b5d0c7d539cf88
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-14 21:12:42 -0500

    finding errors

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,7 +5,7 @@ document.querySelector("body").style.backgroundColor= "red";
 
 //This will be about how many people to invite to a party vs. how many have already been invited.  Max occupancy is 200 people.  I have only x friends.  How many people do I have left to invite.  Based on how many friends you have will be the number of people you will need to invite, all the while making sure you don't go over the max amount of people for the club.  Whew!  Hope I explained that well.
 
-var howManyFriendsYouHave = 200;
+var howManyFriendsYouHave = 600;
 var invited = 80;
 var maxAllowed = 200;
 var stillNeedToInvite = maxAllowed - invited;
@@ -18,5 +18,5 @@ else if (totalInvites -= 199) {
 	console.log("I have " + howManyFriendsYouHave + " friends. I have already invited " + invited + " people to my party, so that means I have " + stillNeedToInvite + " people left to invite. Your " + totalInvites + " exceeds the amount maximum occupancy rull of " + maxAllowed + " .  You will need to invite less people.");
 }
 else {
-	console.log("You have now friends.";
+	console.log("You have now friends.");
 }
