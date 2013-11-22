f617e620d019e0b57a27ce74f65c1b5c8a6719c9
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 23:34:12 -0500

    fixed some errors

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,10 +5,17 @@ document.querySelector("body").style.backgroundColor= "yellow";
 
 //This is a function with else if statements showing how much it will cost for web design services.
 
-function webServices (){
-	var webPages = 2;
+function webServices ()
+{
+	var webPages = 28;
 	var eachPageCosts = 30;
 	var totalCost = webPages * eachPageCosts;
-	console.log("You want a website created with " + webPages + " web pages.  Each page costs " + eachPageCosts + " dollars, so the cost of your website will be, only " + totalCost + " dollars!");
+	var discount = 20;
+	if (webPages <= 20){
+	console.log("You want a website created with " + webPages + " web pages.  Each page costs " + eachPageCosts + " dollars, so the cost of your website will be only " + totalCost + " dollars! You do not qualify for the " + discount + " discount");
 }
-webServices()
+	else{
+		console.log("You want a website created with " + webPages + " web pages.  Each page costs " + eachPageCosts + " dollars, so the cost of your website will be, only " + totalCost + " dollars! You also qualify for the " + discount + " percent discount!");
+	}
+}
+webServices();
