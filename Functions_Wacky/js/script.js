5d59af92e36d56ac356c876bc0328625adbe89f0
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 23:06:18 -0500

    FINALLY I am getting this

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,10 +6,9 @@ document.querySelector("body").style.backgroundColor= "yellow";
 //ARE WE THERE YET? this will show how many miles one can go on how many gallons of gas they have.  Also their fuel economy.
 
 function milesToGo (){
-	var milesDriven;
+	var milesDriven = 12;
 	var totalMiles = 25;
 	var milesLeft = milesDriven - totalMiles;
-	
-	
+	console.log("You drove " + milesDriven + "You have " + milesLeft + "miles left");
 }
-milesToGo(12, 14)
+milesToGo()
