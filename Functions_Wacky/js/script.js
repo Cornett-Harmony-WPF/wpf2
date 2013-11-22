7a10d832adad6afd083665f35bf37805fb71c9f9
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 23:08:08 -0500

    fixed description

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,7 +3,7 @@
 //this changes the css of your body tag to be yellow
 document.querySelector("body").style.backgroundColor= "yellow";
 
-//ARE WE THERE YET? this will show how many miles one can go on how many gallons of gas they have.  Also their fuel economy.
+//ARE WE THERE YET? This is a function that shows how many miles they have driven and how many miles left to go for their road trip.  It is a total of 25 miles long.
 
 function milesToGo (){
 	var milesDriven = 12;
