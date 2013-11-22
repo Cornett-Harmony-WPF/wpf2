1b9b6b3ce94d38ea6e39577fa72e284d98dd3c37
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 23:12:01 -0500

    Creating my Industry scenerio

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,4 +3,12 @@
 //this changes the css of your body tag to be yellow
 document.querySelector("body").style.backgroundColor= "yellow";
 
+//This is a function with else if statements showing how much it will cost for web design services.
 
+function milesToGo (){
+	var milesDriven = 12;
+	var totalMiles = 25;
+	var milesLeft = totalMiles - milesDriven;
+	console.log("You drove " + milesDriven + " miles. You have " + milesLeft + " miles left");
+}
+milesToGo()
