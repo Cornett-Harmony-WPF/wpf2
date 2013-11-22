c1ca5968fa00d61336c510e9668ebfb7664dc91d
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 22:13:00 -0500

    yarnball scenerio created

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,9 +4,10 @@
 document.querySelector("body").style.backgroundColor= "yellow";
 //This statement will be a ternary with a function with if and else.  The scenerio is about yarn and knitting.  How many balls of yarn will I need if I am knitting a blanket with any given length and width.  If 1 ball of yarn = 3 yards or 9 feet of material.
 
-function yarnBalls() {
+function yarnBalls(1, 2, 3, 4, 5, 6) {
     var oneBall = 9;
     var blanketSize;
+	var yarnBall = 
     if (blanketSize >= 9){
         console.log("Your blanket is " + blanketSize + " You will need " + yarnBalls + " balls of yarn");
     } else if(blanketSize <= 9) {
