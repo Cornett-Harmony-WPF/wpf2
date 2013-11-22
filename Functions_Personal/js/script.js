f61cc078dca8668ac5f2444b24dd8daaa38de856
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 22:00:55 -0500

    creating scenerios

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,7 +2,20 @@
 
 //this changes the css of your body tag to be yellow
 document.querySelector("body").style.backgroundColor= "yellow";
+//This statement will be a ternary with a function with if and else.  The scenerio is about yarn and knitting.  How many balls of yarn will I need if I am knitting a blanket with any given length and width.  If 1 ball of yarn = 3 yards or 9 feet of material.
 
+function yarnBalls() {
+    var oneBall = 9;
+    var area = w * h;
+    if (area >= 9){
+        console.log();
+    } else {
+        addItem($this);
+    }
+}
 
-
-
+function updateItem() {
+    $this = $(this);
+    var IsChecked = $this.hasClass("IsChecked");
+    (IsChecked == true) ? removeItem($this) : addItem($this);
+}
