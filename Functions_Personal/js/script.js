c91a28695395810b0930f4db95aa3fa12aa023ed
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 20:40:09 -0500

    creating format

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,6 +1,8 @@
-// JavaScript
+// Functions_Personal Harmony Cornett November 21, 2013
 
 //this changes the css of your body tag to be yellow
 document.querySelector("body").style.backgroundColor= "yellow";
+
+
 
 
