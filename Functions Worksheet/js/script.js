3219ecc231eaf873f2bb2b3fb21e69faa5436987
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 20:30:56 -0500

    finishing examples

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -2,3 +2,19 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF11";
+
+
+//Example:
+
+//Problem: Calculate the area of a rectangle given the width and height of the rectangle.
+//Calculate the Area of a Rectangle
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log("The Area of the Rectangle is " + area);
+
+calculateArea(w, h)
+{
+     area(w *h);
+}
+ 
