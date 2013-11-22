66ac19afdd7b01dd249600ac5f7b727845c797d2
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 20:35:27 -0500

    creating homework assignment functions

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,4 +1,6 @@
 // JavaScript
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF11";
+//this changes the css of your body tag to be yellow
+document.querySelector("body").style.backgroundColor= "yellow";
+
+
