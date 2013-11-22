ad84c98a8a27c8dfb585bf5164a9ec72b4bc5d22
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 22:35:20 -0500

    creating my wacky scenerio

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,6 +1,6 @@
-// JavaScript
+// FUNCTIONS - WACKY  Harmony Cornett November 21, 2013
 
 //this changes the css of your body tag to be yellow
 document.querySelector("body").style.backgroundColor= "yellow";
 
-
+//this will show ...
