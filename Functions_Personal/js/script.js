9bb50a3949e9447e090c8500947e1a8762cc3dce
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 23:50:45 -0500

    fixing errors

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,18 +2,17 @@
 
 //this changes the css of your body tag to be yellow
 document.querySelector("body").style.backgroundColor= "yellow";
-//This statement will be a ternary with a function with if and else.  The scenerio is about yarn and knitting.  How many balls of yarn will I need if I am knitting a blanket with any given length and width.  If 1 ball of yarn = 3 yards or 9 feet of material.
+//This statement will be a ternary with a function with if and else.  The scenerio is about yarn and knitting.  How many balls of yarn will I need if I am knitting a blanket with any given amount of feet of blanket size. 1 ball of yarn = 3 yards or 9 feet of material.
 
-function yarnBalls() {
-    var oneBall = 9;
-    var blanketSize;
-	var yarnBalls = blanketSize * oneBall; 
-    if (blanketSize >= oneBall){
-        console.log("Your blanket is " + blanketSize + " You will need " + yarnBalls + " balls of yarn");
-    } else if(blanketSize <= oneBall) {
-        console.log("Your blanket is " + blanketSize + " You will need " + yarnBalls + " balls of Yarn");
-		return blanketSize;
-		return oneBall;
-		return yarnBalls;
+function yarnBalls () 
+{
+    var oneBall = 3;
+    var blanketSize = 3;
+	var yarnBalls = oneBall * blanketSize; 
+    if (blanketSize >= oneBall) {
+        console.log("Your blanket is " + blanketSize + " feet.  You will need " + yarnBalls + " balls of yarn");
+    } else{
+        console.log("Your blanket is too large, consider knitting a smaller blanket");
 	}
-	
+}
+yarnBalls();
