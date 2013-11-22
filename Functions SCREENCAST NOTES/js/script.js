f3b50a572f9e07497fa945872d37559ab35a3eb3
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 19:54:23 -0500

    screencast notes multiple videos

---
+++ Functions SCREENCAST NOTES/js/script.js
@@ -0,0 +1,27 @@
+// JavaScript- Basic Structure = function invocation
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "white";
+
+
+function outputMsg(){
+	console.log("Hello World");
+}
+
+outputMsg();
+
+
+
+
+//var width = 5;
+
+//function definition
+function calcArea(){
+	var width = 20
+	var height = 30
+	var area = width * height;
+	console.log(area);
+}
+//console.log(width);
+//function call
+calcArea();
