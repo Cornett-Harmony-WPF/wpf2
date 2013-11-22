ebe9f529fc39df0e962f4f3502cbab2bd8889056
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 22:14:06 -0500

    figuring out the syntax

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,10 +4,10 @@
 document.querySelector("body").style.backgroundColor= "yellow";
 //This statement will be a ternary with a function with if and else.  The scenerio is about yarn and knitting.  How many balls of yarn will I need if I am knitting a blanket with any given length and width.  If 1 ball of yarn = 3 yards or 9 feet of material.
 
-function yarnBalls(1, 2, 3, 4, 5, 6) {
+function yarnBalls() {
     var oneBall = 9;
     var blanketSize;
-	var yarnBall = 
+	var yarnBall = BlanketSize * 9; 
     if (blanketSize >= 9){
         console.log("Your blanket is " + blanketSize + " You will need " + yarnBalls + " balls of yarn");
     } else if(blanketSize <= 9) {
