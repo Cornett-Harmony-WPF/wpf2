ca8500c326b2ebaa17d178c66a0cf476b79ddad1
Cornett-Harmony-WPF <harmony@fullsail.edu>
2013-11-21 20:17:02 -0500

    more and more notes

--- Functions SCREENCAST NOTES/js/script.js
+++ Functions SCREENCAST NOTES/js/script.js
@@ -1,45 +1,13 @@
 // JavaScript- Basic Structure = function invocation = Arguments and Parameters
-
-//this changes the css of your body tag to be green
+//procedures and Anonymous Functions
+//this changes the css of your body tag to be white
 document.querySelector("body").style.backgroundColor= "white";
 
 
-function outputMsg(){
-	console.log("Hello World");
-}
+var total = calcArea(30, 20);
 
-outputMsg();
-
-
-
-
-//var width = 5;
-
-//function definition
-function calcArea(){
-	var width = 20
-	var height = 30
-	var area = width * height;
-	console.log(area);
-}
-//console.log(width);
-//function call
-calcArea();
-
-//functions with parameters
-//function call with arguments inside ()
-funcName (argument1, argument2);
-
-//function with parameters
-function funcName (parameter1, parameter2) {
-	//code the function runs
-}
-
-calcArea(30, 20);
-
-function calcArea(w, h){//w=30, h=20
+function calcArea(w, h){
 	var area = w * h;
+	return area;
 }
-//passing value (4,5) to age
-dogYears(4);
-dogYears(5);
+console.log(total);
